5cb80010c2538d864beec10e5deadffc0d622f34
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2022-06-13 16:56:11 +0530

    BAH-1809 | Configuration to create database and its users (#9)
    * BAH-1809 | add helm chart for db setup
    
    - Added a helm chart to deploy the job
    - Added a github workflow for deploying the job when triggered
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | add steps to delete resources after completion
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | add feature branch to trigger
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * Fix. Job name for db setup
    
    CCo-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | refactor. remove feature branch from workflow trigger
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | Moved db-setup helm chart to a new repo
    
    * BAH-1809 | Add user input for db setup step
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | add masking for ssm parameters
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | refactor step name and orders
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | fix. dependency list command
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | refactor. shell script for setting SSM secrets
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>

---
+++ .github/setupEnvSecrets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+function exportWithMask {
+    SSM_PARAMETER_NAME=$1
+    ENV_VARIABLE_NAME=$2
+
+    PARAMETER_VALUE=$(aws ssm get-parameter --with-decryption --name "$SSM_PARAMETER_NAME" --query "Parameter.Value" --output text)
+    echo "::add-mask::$PARAMETER_VALUE"
+    echo "$ENV_VARIABLE_NAME=$PARAMETER_VALUE" >> $GITHUB_ENV
+}
+
+ENVIRONMENT=$1
+
+exportWithMask "/$ENVIRONMENT/rds/mysql/host" 'RDS_HOST'
+exportWithMask "/$ENVIRONMENT/rds/mysql/username" 'RDS_USERNAME'
+exportWithMask "/$ENVIRONMENT/rds/mysql/password" 'RDS_PASSWORD'
+exportWithMask "/$ENVIRONMENT/openmrs/DB_USERNAME" 'OPENMRS_DB_USERNAME'
+exportWithMask "/$ENVIRONMENT/openmrs/DB_PASSWORD" 'OPENMRS_DB_PASSWORD'
+exportWithMask "/$ENVIRONMENT/reports/DB_USERNAME" 'REPORTS_DB_USERNAME'
+exportWithMask "/$ENVIRONMENT/reports/DB_PASSWORD" 'REPORTS_DB_PASSWORD'
+exportWithMask "/$ENVIRONMENT/crater/DB_USERNAME" 'CRATER_DB_USERNAME'
+exportWithMask "/$ENVIRONMENT/crater/DB_PASSWORD" 'CRATER_DB_PASSWORD'
+exportWithMask "/nonprod/rabbitmq/USERNAME" 'MQ_USERNAME'
+exportWithMask "/nonprod/rabbitmq/PASSWORD" 'MQ_PASSWORD'
+exportWithMask "/nonprod/psql/DB_PASSWORD" 'PSQL_PASSWORD'
+exportWithMask "/nonprod/abdm/GATEWAY_CLIENT_ID" 'GATEWAY_CLIENT_ID'
+exportWithMask "/nonprod/abdm/GATEWAY_CLIENT_SECRET" 'GATEWAY_CLIENT_SECRET'
